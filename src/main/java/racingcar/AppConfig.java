8ceb05f556d0689b673b4fb66534a5c6428a2231
Seooooo24 <ahyeungii@gmail.com>
2025-04-04 21:06:41 +0900

    feat: 의존성 주입할 AppConfig 생성

---
+++ src/main/java/racingcar/AppConfig.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import racingcar.controller.Controller;
+import racingcar.view.InputView;
+import racingcar.view.TextInputView;
+
+public class AppConfig {
+
+    public Controller controller() {
+        return new Controller(inputView());
+    }
+
+    private InputView inputView() {
+        return new TextInputView();
+    }
+
+}
