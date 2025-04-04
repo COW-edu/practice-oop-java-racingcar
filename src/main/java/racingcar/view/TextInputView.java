9aff35ceae1308997e39820ea2b90e6f964d2335
Seooooo24 <ahyeungii@gmail.com>
2025-04-04 20:30:06 +0900

    feat: view에 입력 안내 문구 추가

--- src/main/java/racingcar/view/TextInputView.java
+++ src/main/java/racingcar/view/TextInputView.java
@@ -4,13 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class TextInputView implements InputView {
 
+    public static final String INPUT_RACING_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_GAME_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+
     @Override
     public String inputRacingCarsNames() {
+        System.out.println(INPUT_RACING_CARS_NAME_MESSAGE);
         return Console.readLine();
     }
 
     @Override
     public String inputGameCount() {
+        System.out.println(INPUT_GAME_COUNT_MESSAGE);
         return Console.readLine();
     }
 }
