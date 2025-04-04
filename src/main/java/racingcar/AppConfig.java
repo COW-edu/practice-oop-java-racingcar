2cd1a849da6aefca4371f9682125f320c6754e58
Seooooo24 <ahyeungii@gmail.com>
2025-04-04 21:51:55 +0900

    feat: AppConfig에서 RacingCarService 주입

--- src/main/java/racingcar/AppConfig.java
+++ src/main/java/racingcar/AppConfig.java
@@ -1,17 +1,22 @@
 package racingcar;
 
 import racingcar.controller.Controller;
+import racingcar.model.RacingCarService;
+import racingcar.model.RacingCarServiceImpl;
 import racingcar.view.InputView;
 import racingcar.view.TextInputView;
 
 public class AppConfig {
 
     public Controller controller() {
-        return new Controller(inputView());
+        return new Controller(inputView(), racingCarService());
     }
 
     private InputView inputView() {
         return new TextInputView();
     }
 
+    private RacingCarService racingCarService() {
+        return new RacingCarServiceImpl();
+    }
 }
