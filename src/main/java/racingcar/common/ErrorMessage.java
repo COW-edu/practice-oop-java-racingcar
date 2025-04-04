edb68dcf290c55161cc216d5d80912b4c1391961
Seooooo24 <ahyeungii@gmail.com>
2025-04-04 21:07:18 +0900

    feat: 에러메시지 관리 클래스 생성

---
+++ src/main/java/racingcar/common/ErrorMessage.java
@@ -0,0 +1,7 @@
+package racingcar.common;
+
+public class ErrorMessage {
+
+    public static final String ERROR_EMPTY_RACING_CAR_NAME = "[ERROR] 입력된 자동차 이름이 없습니다.";
+    public static final String ERROR_SINGLE_RACING_CAR_NAME = "[ERROR] 한 대의 자동차로는 경기할 수 없습니다.";
+}
