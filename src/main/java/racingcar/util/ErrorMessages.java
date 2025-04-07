c965d4075cd9157faa47b3e820ee8536a20da349
JinHyeon <tiemo0708@naver.com>
2025-04-07 13:45:17 +0900

    feat: 자동차 하나의 이름과 위치를 가지는 객체 생성

---
+++ src/main/java/racingcar/util/ErrorMessages.java
@@ -0,0 +1,9 @@
+package racingcar.util;
+
+public class ErrorMessages {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    public static final String INVALID_CAR_NAME_BLANK = ERROR_PREFIX + "자동차 이름은 비어 있을 수 없습니다.";
+    public static final String TOO_LONG_CAR_NAME = ERROR_PREFIX + "자동차 이름은 5자 이하여야 합니다.";
+
+}
