d53fc0fbe5abbf24e36846a637c5087d230ed656
JinHyeon <tiemo0708@naver.com>
2025-04-07 14:58:59 +0900

    feat: 자동차 여러개를 집합으로 관리하는 일급컬렉션 추가

--- src/main/java/racingcar/util/ErrorMessages.java
+++ src/main/java/racingcar/util/ErrorMessages.java
@@ -5,5 +5,6 @@ public class ErrorMessages {
 
     public static final String INVALID_CAR_NAME_BLANK = ERROR_PREFIX + "자동차 이름은 비어 있을 수 없습니다.";
     public static final String TOO_LONG_CAR_NAME = ERROR_PREFIX + "자동차 이름은 5자 이하여야 합니다.";
+    public static final String DUPLICATE_CAR_NAME = ERROR_PREFIX + "중복된 자동차 이름이 존재합니다.";
 
 }
