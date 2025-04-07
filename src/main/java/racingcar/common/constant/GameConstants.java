3d6d87ae47d3a489d0f9a8b9fca2b1de9606bd1b
SeungGyu <phillipskl123@gmail.com>
2025-04-07 16:43:26 +0900

    [feat] 자동차 이름 길이 검증 로직 추가

--- src/main/java/racingcar/common/constant/GameConstants.java
+++ src/main/java/racingcar/common/constant/GameConstants.java
@@ -5,6 +5,7 @@ public class GameConstants {
     public static final int MIN_MOVE_THRESHOLD = 4;
     public static final int RANDOM_MIN = 0;
     public static final int RANDOM_MAX = 9;
+    public static final int MAX_NAME_LENGTH = 5;
 
     public static final String CAR_NAME_DELIMITER = ",";
 
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,17 +2,27 @@ package racingcar.domain;
 
 import racingcar.dto.CarStatus;
 
+import static racingcar.common.constant.ErrorMessages.*;
+import static racingcar.common.constant.GameConstants.*;
+
 public class Car {
 
     private final String name;
     private int position;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
     }
 
     public void move() {
         position++;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
+        }
     }
 
     public String getName() {
