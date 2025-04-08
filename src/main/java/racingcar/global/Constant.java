f64b6ed9debcfc0a8255054e9add1dda7873a3e0
baaamk <brandon55321@gmail.com>
2025-04-09 02:48:42 +0900

    feat: 상수 클래스 생성
    1. 에러 메세지
    2. 매직 넘버

---
+++ src/main/java/racingcar/global/Constant.java
@@ -0,0 +1,9 @@
+package racingcar.global;
+
+public class Constant {
+    public static int MIN = 0;
+    public static int MAX = 9;
+    public static int MOVE_NUMBER = 4;
+    public static int START_POSITION = 0;
+
+}
---
+++ src/main/java/racingcar/global/ErrorMessage.java
@@ -0,0 +1,14 @@
+package racingcar.global;
+
+public class ErrorMessage {
+    public static final String EMPTY_INPUT = "빈 값이 들어오면 안됩니다.";
+    public static final String INVALID_NUMBER_FORMAT = "정수만 입력 가능합니다.";
+
+    public static final String NAME_TOO_LONG = "이름은 5자 이하여야 합니다.";
+    public static final String NAME_MISSING = "이름이 존재하지 않습니다.";
+    public static final String DUPLICATE_NAME = "중복된 이름이 존재합니다.";
+    public static final String EMPTY_NAME_LIST = "이름 목록이 비어있습니다.";
+
+    public static final String NO_CARS_TO_RACE = "경주할 자동차가 없습니다.";
+    public static final String INVALID_TRY_NUMBER = "시도 숫자는 0보다 커야합니다.";
+}
