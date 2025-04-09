aff080f5cba87642a9112402d33ec17b83995ee4
Suehyun666 <em0200878@gmail.com>
2025-04-09 12:55:58 +0900

    feature: 로직 관리하는 manager 추가

---
+++ src/main/java/racingcar/domain/RaceManager.java
@@ -0,0 +1,48 @@
+package racingcar.domain;
+
+import racingcar.dto.RoundResultDto;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+import racingcar.domain.strategy.MovementStrategy;
+import racingcar.domain.strategy.RandomMovement;
+import racingcar.view.View;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class RaceManager {
+    private final View view;
+    private final MovementStrategy movementStrategy;
+    private Cars cars;
+
+    public RaceManager(View view) {
+        this.view = view;
+        this.movementStrategy = new RandomMovement();
+    }
+
+    public void start() {
+        String[] carNames = view.readCarNames();
+        this.cars = new Cars(Arrays.asList(carNames));
+
+        int attempts = view.readAttempts();
+        view.printRaceStart();
+
+        for (int i = 0; i < attempts; i++) {
+            executeRound();
+        }
+        announceWinners();
+    }
+
+    private void executeRound() {
+        cars.moveAll(movementStrategy);
+        RoundResultDto result = new RoundResultDto(
+                cars.getRacers().stream()
+                        .map(Car::toString)
+                        .collect(Collectors.toList())
+        );
+        view.printRoundResult(result.formatResult());
+    }
+
+    private void announceWinners() {
+        view.printWinners(cars.findWinnerNames());
+    }
+}
---
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -0,0 +1,49 @@
+package racingcar.domain.car;
+
+import racingcar.domain.strategy.MovementStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> racers;
+    private static final int INITIAL_POSITION = 0;
+
+    public Cars(List<String> names) {
+        this.racers = names.stream()
+                .map(name -> new Car(new CarName(name), INITIAL_POSITION))
+                .collect(Collectors.toList());
+    }
+
+    public void moveAll(MovementStrategy strategy) {
+        racers.forEach(car -> car.advance(strategy));
+    }
+
+    public List<String> findWinnerNames() {
+        int maxPosition = findMaxPosition();
+
+        return racers.stream()
+                .filter(car -> car.isAtPosition(maxPosition))
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
+    private int findMaxPosition() {
+        return racers.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Car> getRacers() {
+        return new ArrayList<>(racers);
+    }
+
+    @Override
+    public String toString() {
+        return racers.stream()
+                .map(Car::toString)
+                .collect(Collectors.joining("\n"));
+    }
+}
