da2bfa65240b70adac50db5779e08195d432cccb
Suehyun666 <em0200878@gmail.com>
2025-04-09 12:56:32 +0900

    feature: main 메서드

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+import racingcar.domain.RaceManager;
+import racingcar.view.ConsoleView;
+import racingcar.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            View view = new ConsoleView();
+            RaceManager raceManager = new RaceManager(view);
+            raceManager.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
